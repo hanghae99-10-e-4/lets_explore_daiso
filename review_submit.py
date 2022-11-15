fd9612c22ad464dd74f22db2da41e7a89ac354e5
soojung01 <ksj29622@naver.com>
2022-11-15 21:09:12 +0900

    리뷰 등록페이지 서버 수정

---
+++ review_submit.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, jsonify, request, render_template
+submit_api = Blueprint('submit_api', __name__)
+from db_connect import mongodb
+
+@submit_api.route('/')
+def home():
+    return render_template('index.html')
+
+@submit_api.route('/review-submit', methods=['POST'])
+def review_submit():
+    star_receive = request.form['star_give']
+    price_receive = request.form['price_give']
+    title_receive = request.form['title_give']
+    comment_receive = request.form['comment_give']
+
+    doc = {
+        'star': star_receive,
+        'price': price_receive,
+        'title': title_receive,
+        'comment': comment_receive
+    }
+    mongodb().users.insert_one(doc)
+    return jsonify({'msg': '게시완료!'})
+
