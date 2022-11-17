85665af926257ba2005cb293713d95afa9f8d771
pds0309 <118153183+superariella@users.noreply.github.com>
2022-11-17 15:59:40 +0900

    상세보기 연결 및 이미지 목록 보여주기 추가

--- review_details.py
+++ review_details.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, jsonify
 from db_connect import mongodb
+
 review_details_api = Blueprint('review_details_api', __name__)
 
 
 @review_details_api.route('/api/reviews/<string:id>', methods=['GET'])
 def review_get(id):
-    review_list = mongodb().review.find_one({'id':id},{'_id':False})
-    print(review_list)
+    review_list = mongodb().review.find_one({'id': int(id)}, {'_id': False})
     return jsonify({'review': review_list})
